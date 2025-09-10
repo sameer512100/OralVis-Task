5d341a46fa15cb1266eaa5390afc565d620ecbfa
Sameer Sayyed <sayyedsameer2106@gmail.com>
2025-09-11 00:39:42 +0530

    "cors enabled"

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://medannotate-opal.vercel.app',
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/uploads", (req, res, next) => {
